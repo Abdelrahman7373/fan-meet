c4d22f8da9b6fd3a6071eef96adcd063b41a74c3
Abdelrahman <104079380+Abdelrahman7373@users.noreply.github.com>
2024-08-03 13:12:44 +0300

    Update MeetingTypeList.tsx

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -54,7 +54,7 @@ const MeetingTypeList = () => {
   return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
       <HomeCard img='/icons/add-meeting.svg' title='New Meeting' description='Start an instant meeting' className='bg-orange-1' handleClick={() => setMeetingState('isInstantMeeting')} />
-      <HomeCard img='/icons/Schedule.svg' title='Schedule Meeting' description='Plan your meeting' className='bg-blue-1' handleClick={() => setMeetingState('isScheduleMeeting')} />
+      <HomeCard img='/icons/schedule.svg' title='Schedule Meeting' description='Plan your meeting' className='bg-blue-1' handleClick={() => setMeetingState('isScheduleMeeting')} />
       <HomeCard img='/icons/recordings.svg' title='View Recordings' description="Check out meetings you've recorded" className='bg-purple-1' handleClick={() => router.push('/recordings')} />
       <HomeCard img='/icons/join-meeting.svg' title='Join Meeting' description='Join meeting with invitation link' className='bg-yellow-1' handleClick={() => setMeetingState('isJoiningMeeting')} />
       {!callDetails ? (
